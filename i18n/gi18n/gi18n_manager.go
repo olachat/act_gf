c9f24c8485063711818fee449cecd8b905cd8bbb
yichangpeng-ola <Robin@highsinger.com>
2025-03-13 11:57:39 +0800

    debug

--- i18n/gi18n/gi18n_manager.go
+++ i18n/gi18n/gi18n_manager.go
@@ -328,6 +328,7 @@ func (m *Manager) init() {
 				for k, v := range j.Map() {
 					m.data[lang][k] = gconv.String(v)
 				}
+				intlog.Printf("load i18n file '%s' success, lang: %s", file, lang)
 			} else {
 				intlog.Errorf("load i18n file '%s' failed: %v", file, err)
 			}
@@ -337,6 +338,7 @@ func (m *Manager) init() {
 			// Any changes of i18n files, clear the data.
 			m.mu.Lock()
 			m.data = nil
+			intlog.Printf("i18n gfsnotify event: %+v", event)
 			m.mu.Unlock()
 			gfsnotify.Exit()
 		})
